e3275e83b88496ebd2eb06a1e6f1237b22c44c80
Odjoutchone Sylvestre Tokou <odjoutchone@yahoo.fr>
2023-09-04 20:35:05 -0400

    Add to App

--- src/components/App.js
+++ src/components/App.js
@@ -15,14 +15,22 @@ function Home() {
     </div>
   );
 }
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
 
-{/* write an <About> component here */}
-
+}
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home/>
+      <About/>
+
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
